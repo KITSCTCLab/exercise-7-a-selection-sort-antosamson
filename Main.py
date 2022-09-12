092eee18c999b78dc122740462b012e0bb645198
antosamson <109952669+antosamson@users.noreply.github.com>
2022-09-12 15:21:25 +0530

    2 commit

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
 from typing import List
 
-def selectionSort(array, size) -> List[int]:
-  # Write your code here
-      for i in range(0,size-1):
-        min=i
-        for j in range(i+1,size):
-            if array[j]<aarray[min]:
-                min=j
-        if min !=i:
-            t=array[i]
-            array[i]=array[min]
-            array[min]=t
+def insertionSort(array) -> List[int]:
+  """
+  Function implementing insertion sort algorithm to return the passed array sorted in the ascending order
+  """
+  size = len(array)
+  for index in range(1, size):
+    curr_ind = index
+    while curr_ind > 0 and array[curr_ind - 1] > array[curr_ind]:
+      array[curr_ind - 1], array[curr_ind] = array[curr_ind], array[curr_ind - 1]
+      curr_ind -= 1
+  return array
 
-# Do not change the following code
+# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -20,4 +20,5 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(selectionSort(data, len(data)))
+print(insertionSort(data))
+
